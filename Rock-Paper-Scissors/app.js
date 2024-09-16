adf7c8ba21f4e9ab1caee8310fb0b9d980130095
Md Mustakin Ebne Mazid <mustakinshvn@gmail.com>
2024-09-17 02:55:55 +0600

    Completed

---
+++ Rock-Paper-Scissors/app.js
@@ -0,0 +1,64 @@
+let userScore = 0;
+let computerScore = 0;
+const choices = document.querySelectorAll('.choice');
+const message = document.querySelector('#msg');
+const userScr = document.querySelector('#user-score');
+const compScr = document.querySelector('#comp-score');
+const compChoose = document.querySelector('#computerChoose');
+
+const drawGame = () => {
+    console.log("It's a tie");
+    message.innerText = "It's a tie!";
+}
+
+const winGame = () => {
+    userScore++;
+    console.log("You win");
+    userScr.innerText = userScore;
+    message.innerText = "You win!";
+
+}
+
+const loseGame = () => {       
+    computerScore++; 
+    console.log("You lose");
+    compScr.innerText =  computerScore;
+    message.innerText = "You lose!";
+}
+
+const compChoice = () =>{
+    options = ['rock', 'paper', 'scissors'];
+    const randomNum = Math.floor(Math.random() * 3);
+    return options[randomNum];
+}
+
+
+const playGame = (userChoice) => {
+     console.log("User's choices " , userChoice);
+     const computerChoice = compChoice();
+     compChoose.innerText = "Computer choose " + computerChoice + "!";
+     console.log("Computer's choices " , computerChoice);
+
+     if(userChoice === computerChoice){
+         drawGame();    
+     }else{
+            if(userChoice === 'rock' && computerChoice === 'scissors'){
+                winGame();
+            }else if(userChoice === 'paper' && computerChoice === 'rock'){
+                winGame();
+            }else if(userChoice === 'scissors' && computerChoice === 'paper'){
+                winGame();
+            }else{
+                loseGame();
+            }
+     }
+
+}
+ 
+choices.forEach((choice) => {
+    choice.addEventListener("click", ()=> {
+        const userChoice = choice.getAttribute('id');
+      playGame(userChoice);
+    });
+
+});
